eedf5cf9bd5c1771483db9ef37512ed7b4a1eebb
Edurb23 <eddurb64@gmail.com>
2026-03-16 23:31:44 -0300

    Implementar Validações nos formularios de editar e criar

--- src/app/componentes/vinyls/editar-vinyl/editar-vinyl.component.ts
+++ src/app/componentes/vinyls/editar-vinyl/editar-vinyl.component.ts
@@ -2,7 +2,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Vinyl } from '../vinyl';
 import { VinylService } from '../vinyl.service';
-import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-vinyl',
@@ -10,45 +10,67 @@ import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
   styleUrls: ['./editar-vinyl.component.css'],
 })
 export class EditarVinylComponent implements OnInit {
+  vinyl!: FormGroup;
 
-
-  vinyl!: FormGroup
-
-
-
-
-  constructor(private service: VinylService, private router: Router, private route: ActivatedRoute, private formBuilder: FormBuilder ) { }
+  constructor(
+    private service: VinylService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+  ) {}
 
   ngOnInit(): void {
+    this.vinyl = this.formBuilder.group({
+      id: [0],
+      name: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/),
+          Validators.minLength(2),
+        ]),
+      ],
+      artist: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(2),
+          Validators.pattern(/(.|\s)*\S(.|\s)*/),
+        ]),
+      ],
+      year: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.min(1948),
+          Validators.max(new Date().getFullYear()),
+          Validators.pattern(/^\d{4}$/),
+        ]),
+      ],
+      photo: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(/^(https?:\/\/)[^\s$.?#].[^\s]*$/),
+        ]),
+      ],
+    });
 
-      this.vinyl = this.formBuilder.group({
-        id: [0],
-        name: [''],
-        artist: [''],
-        year: [0],
-        photo: ['']
-      })
-
-
-      const id = this.route.snapshot.paramMap.get('id')
-      this.service.buscarPorId(parseInt(id!)).subscribe((vinil) => {
-        this.vinyl.patchValue(vinil)
-      })
-
+    const id = this.route.snapshot.paramMap.get('id');
+    this.service.buscarPorId(parseInt(id!)).subscribe((vinil) => {
+      this.vinyl.patchValue(vinil);
+    });
   }
 
-
-  editarVinyl(){
+  editarVinyl() {
     this.service.editar(this.vinyl.value).subscribe(() => {
       this.router.navigate(['']).then(() => {
         window.location.reload();
-      })
-    })
-
+      });
+    });
   }
 
-  cancelar(){
-     this.router.navigate([''])
+  cancelar() {
+    this.router.navigate(['']);
   }
-
 }
